b471ece54f7c31f4d7d21c189bc2957e4c82f73a
JIHOON PARK <ampo2787@cnu.ac.kr>
2018-12-29 20:28:50 +0900

    Initial Commit

---
+++ swiftPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swiftPractice
+//
+//  Created by JihoonPark on 29/12/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
